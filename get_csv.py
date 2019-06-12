d7ac8fd17b68239422d61cd4b5bf954b41ea05f3
cpd-cemtral <jmarsnik@ceg-engineers.com>
2019-06-12 18:46:53 -0500

    removed f strings

--- get_csv.py
+++ get_csv.py
@@ -84,13 +84,13 @@ def get_csv(get_options=False):
             time.sleep(1)
             old_exists = os.path.isfile(download_dir + '/{}'.format(old_filename))
         
-        new_exists = os.path.isfile(download_dir + f'/{new_filename}')
+        new_exists = os.path.isfile(download_dir + '/{}'.format(old_filename))
         #second while loop to check if file name has been changed
         while not new_exists:
             print('in second while')
             if old_exists:
-                os.rename(download_dir + f'/{old_filename}',
-                        download_dir + f'/{new_filename}')
+                os.rename(download_dir + '/{}'.format(old_filename),
+                        download_dir + '/{}'.format(old_filename))
                 time.sleep(1) 
                 break
             else:
